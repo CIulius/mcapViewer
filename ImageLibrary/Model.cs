b49b3ad418c6a695c73647ea4529c38865e72494
Paul-Cristian <brinzapaul@gmail.com>
2019-04-25 21:58:16 +0300

    Added some logic for next and previous image in Model module

---
+++ ImageLibrary/Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageLibrary
+{
+    public class Model : IModel
+    {
+        private string currentImagePath;
+        private List<string> imagesList;
+
+        public Model()
+        {
+            imagesList = new List<string> { "one", "two", "three" };
+            currentImagePath = imagesList.First();
+        }
+
+        public string Path { get { return currentImagePath; } }
+        public void showNextImage()
+        {
+            int index = -1;
+            bool finded = false;
+
+            while (index < imagesList.Count && finded != true)
+            {
+                index++;
+
+                if (imagesList[index].Equals(currentImagePath))
+                {
+                    index++;
+                    index %= imagesList.Count;
+
+                    currentImagePath = imagesList[index];
+                    finded = true;
+                }
+            }
+        }
+
+        public void showPreviousImage()
+        {
+            int index = imagesList.Count;
+            bool finded = false;
+
+            while (index > 0 && finded != true)
+            {
+                index--;
+
+                if (imagesList[index].Equals(currentImagePath))
+                {
+                    index--;
+                    if (index < 0)
+                    {
+                        index = imagesList.Count - 1;
+                    }
+                    currentImagePath = imagesList[index];
+                    finded = true;
+                }
+            }
+        }
+    }
+}
---
+++ ImageLibraryTest/UnitTest_Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageLibrary;
+
+namespace ImageLibraryTest
+{
+    [TestClass]
+    public class UnitTest_Model
+    {
+        [TestMethod]
+        public void Test_showNextImageMethod()
+        {
+            Model m = new Model();
+            Assert.AreEqual(m.Path, "one");
+
+            m.showNextImage();
+            Assert.AreEqual(m.Path, "two");
+
+            m.showNextImage();
+            Assert.AreEqual(m.Path, "three");
+
+            m.showNextImage();
+            Assert.AreEqual(m.Path, "one");
+        }
+
+        [TestMethod]
+        public void Test_showPreviousImageMethod()
+        {
+            Model m = new Model();
+            Assert.AreEqual(m.Path, "one");
+
+            m.showPreviousImage();
+            Assert.AreEqual(m.Path, "three");
+
+            m.showPreviousImage();
+            Assert.AreEqual(m.Path, "two");
+
+            m.showPreviousImage();
+            Assert.AreEqual(m.Path, "one");
+        }
+    }
+}
